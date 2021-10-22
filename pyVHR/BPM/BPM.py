84df49610cb91b420317bff00b84d7ae70bac515
Alessandro D'Amelio <ale.damelio92@gmail.com>
2021-10-22 12:51:10 +0200

    fixed MAD import and stats init

--- pyVHR/BPM/BPM.py
+++ pyVHR/BPM/BPM.py
@@ -7,6 +7,7 @@ import plotly.graph_objects as go
 from scipy.interpolate import interp1d
 from pyVHR.plot.visualize import VisualizeParams
 from pyVHR.BPM.utils import *
+from scipy.stats import median_absolute_deviation as mad
 
 """
 This module contains all the class and the methods for transforming 
