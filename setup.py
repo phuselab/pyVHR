32f3d39c47b980c47e381c638ed43416b24a550b
vcuculo <vittorio@dotdotdot.it>
2021-10-28 18:14:19 +0200

    bump up version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyVHR",
-    version="1.0.1",
+    version="1.0.2",
     author="Vittorio Cuculo <vittorio.cuculo@unimi.it>, Donatello Conte <donatello.conte@univ-tours.fr>, Alessandro D'Amelio <alessandro.damelio@unimi.it>, Giuliano Grossi <giuliano.grossi@unimi.it>, Edoardo Mortara <edoardo.mortara@studenti.unimi.it>",
     author_email="",
     description="Package for rPPG methods",
