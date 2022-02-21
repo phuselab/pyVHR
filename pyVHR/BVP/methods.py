095aeb0b05571f804e695cd272d0ee2b03bf5834
Arritmic <arritmic@gmail.com>
2022-02-21 23:20:22 +0200

    Fixed OMIT method to process several patches.

--- pyVHR/BVP/methods.py
+++ pyVHR/BVP/methods.py
@@ -256,14 +256,18 @@ def cpu_OMIT(signal):
 
     Álvarez Casado, C., Bordallo López, M. (2022). Face2PPG: An unsupervised pipeline for blood volume pulse extraction from faces. arXiv (eprint 2202.04101).
     """
-    X = signal
-    Q, R = np.linalg.qr(X)
-    S = Q[:, 0].reshape(1, -1)
-    P = np.identity(3) - np.matmul(S.T, S)
-    Y = np.dot(P, X)
-    bvp = Y[1, :]
+
+    bvp = []
+    for i in range(signal.shape[0]):
+        X = signal[i]
+        Q, R = np.linalg.qr(X)
+        S = Q[:, 0].reshape(1, -1)
+        P = np.identity(3) - np.matmul(S.T, S)
+        Y = np.dot(P, X)
+        bvp.append(Y[1, :])
+    bvp = np.array(bvp)
     return bvp
-    
+
     
 def cpu_ICA(signal, **kargs):
     """
