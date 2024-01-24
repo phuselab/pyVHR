2a646c19f7e751ddb6bcca27675c852cdfc82012
Alessandro Chiesi <alecsoe94@gmail.com>
2024-01-24 18:12:54 +0100

    minor fixes

--- pyVHR/deepRPPG/transformer.py
+++ pyVHR/deepRPPG/transformer.py
@@ -1,13 +1,16 @@
 import pyVHR
 import numpy as np
+import os
 import torch
 import time
 import torchvision.transforms as transforms
 
 from torch.utils.data import DataLoader
 
-from .TRANSFORMER import rPPGTransformer
+from .TRANSFORMER.model import rPPGTransformer
+from .HR_CNN.utils import butter_bandpass_filter
 
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 def RPPG_TRANSFORMER_bvp_pred(frames):
     model_path = pyVHR.__path__[0] + "/deepRPPG/TRANSFORMER/model_vipl_next_50_1.dct"
@@ -22,29 +25,41 @@ def RPPG_TRANSFORMER_bvp_pred(frames):
 
 
     model.eval()
-    print(frames.shape)
-    data_loader = DataLoader(frames, transforms=transforms.ToTensor())
+
+    frames = torch.as_tensor(frames)
+    frames = torch.utils.data.TensorDataset(frames.to(device, dtype=torch.float))
+    frames_loader = torch.utils.data.DataLoader(frames,
+                                                batch_size=16,
+                                                shuffle=True,
+                                                drop_last=True)
+
+    start = time.time()
 
     outputs = []
 
-    start = time.time()
-    for i, chrom_patches in enumerate(data_loader):
-        output = model(chrom_patches)
+    for i, chrom_patches in enumerate(frames_loader):
+        output = model(chrom_patches[0]).detach().squeeze().cpu().numpy()
         outputs.append(output)
     end = time.time()
     print("processing time: ", end - start)
-
-    outputs = (outputs - torch.mean(outputs)) / torch.std(outputs)
-    outputs = outputs.tolist()
+    
+    outputs=np.array(outputs)
+    
+    mean = np.mean(outputs)
+    outputs = (outputs - np.mean(outputs)) / np.std(outputs)
 
     fs = 30
+
     lowcut = 0.8
     highcut = 6
-
+    
     filtered_outputs = butter_bandpass_filter(outputs, lowcut, highcut, fs, order=4)
     filtered_outputs = (filtered_outputs - np.mean(filtered_outputs)) / np.std(filtered_outputs)
 
-    return np.array(filtered_outputs)
+    # rearrange output unbatched
+    filtered_outputs = np.array(filtered_outputs)
+    filtered_outputs = np.concatenate(np.concatenate(filtered_outputs, axis=0), axis=0)
 
+    return filtered_outputs
     
     
